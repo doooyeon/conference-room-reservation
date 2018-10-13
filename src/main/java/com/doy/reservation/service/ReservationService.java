ff61c14015e6032add245c62d83775160fff4cbf
doooyeon <ehdus5958@naver.com>
2018-10-14 04:40:46 +0900

    Update duplicate check recursion condition

--- src/main/java/com/doy/reservation/service/ReservationService.java
+++ src/main/java/com/doy/reservation/service/ReservationService.java
@@ -21,14 +21,14 @@ public class ReservationService {
     public void save(ReservationDTO reservationDTO) {
         checkDuplicate(reservationDTO);
 
-        for (int i = 0; i <= reservationDTO.getNumOfRecursion(); i++) {
+        for (int i = 0; i < reservationDTO.getNumOfRecursion(); i++) {
             reservationDTO.setDate(reservationDTO.getDate().plusWeeks(i));
             reservationRepository.save(reservationDTO.toEntity());
         }
     }
 
     public void checkDuplicate(ReservationDTO reservationDTO) {
-        for (int i = 0; i <= reservationDTO.getNumOfRecursion(); i++) {
+        for (int i = 0; i < reservationDTO.getNumOfRecursion(); i++) {
             ReservationDTO dto = new ReservationDTO(reservationDTO.getRoomName(), reservationDTO.getReservedName(), reservationDTO.getDate().plusWeeks(i), reservationDTO.getStartTime(), reservationDTO.getEndTime(), reservationDTO.getNumOfRecursion());
             List<Reservation> reservations = reservationRepository.findByDateAndRoomName(reservationDTO.getDate(), reservationDTO.getRoomName());
 
