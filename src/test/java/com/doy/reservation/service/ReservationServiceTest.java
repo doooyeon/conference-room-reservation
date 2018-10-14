786043dfc7d3cb2929ebed235ee3765e16211fae
doooyeon <ehdus5958@naver.com>
2018-10-14 22:40:32 +0900

    Update page title

--- src/test/java/com/doy/reservation/service/ReservationServiceTest.java
+++ src/test/java/com/doy/reservation/service/ReservationServiceTest.java
@@ -15,7 +15,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
