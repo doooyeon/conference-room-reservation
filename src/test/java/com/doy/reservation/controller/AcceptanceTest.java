c5374fcdd9e7f7530a75e8ec9dc36917bd53534b
doooyeon <ehdus5958@naver.com>
2018-10-15 01:48:10 +0900

    Add spring profile

--- src/test/java/com/doy/reservation/controller/AcceptanceTest.java
+++ src/test/java/com/doy/reservation/controller/AcceptanceTest.java
@@ -6,10 +6,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.*;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 public abstract class AcceptanceTest {
     @Autowired
     private TestRestTemplate template;
